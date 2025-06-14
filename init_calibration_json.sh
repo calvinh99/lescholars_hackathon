2ccb4700e0371c28cee4337a7bd143877fc37103
Bill Fang <will062m@gmail.com>
2025-06-14 15:31:52 -0700

    update init_cal shell

--- init_calibration_json.sh
+++ init_calibration_json.sh
@@ -1,2 +1,5 @@
 FOLLOWER_ARM_DEST=~/.cache/huggingface/lerobot/calibration/robots/so101_follower/lescholars_follower_arm.json
+LEADER_ARM_DEST=~/.cache/huggingface/lerobot/calibration/teleoperators/so101_leader/lescholars_leader_arm.json
+
 cp ./calibration_jsons/lescholars_follower_arm.json $FOLLOWER_ARM_DEST
+cp ./calibration_jsons/lescholars_leader_arm.json $LEADER_ARM_DEST
